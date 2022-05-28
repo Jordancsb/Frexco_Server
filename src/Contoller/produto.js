4681b8c70912af345021b644a52a5d2b8a2a44f8
Jordancsb <jcaindoor@gmail.com>
2022-05-28 15:30:35 -0300

    feat: create method update, post e delete

--- src/Contoller/produto.js
+++ src/Contoller/produto.js
@@ -11,3 +11,31 @@ export async function insertProduto(produto){
     db.run('INSERT INTO produto (nome, idade) VALUES (?, ?)', [produto.nome, produto.idade])
   })
 }
+
+export async function updateProduto(produto){
+  openDb().then(db=>{
+    db.run('UPDATE produto SET nome=?, idade=? WHERE id=?', [produto.nome, produto.idade, produto.id])
+  })
+}
+
+export async function selectProdutos(){
+  return openDb().then(db=>{
+    return db.all('SELECT * FROM produto')
+    .then(res => res)
+  })
+}
+
+export async function selectProduto(id){
+  return openDb().then(db=>{
+    return db.get('SELECT * FROM produto WHERE id=?', [id])
+    .then(res => res)
+  })
+}
+
+export async function deleteProduto(id){
+  return openDb().then(db=>{
+    return db.get('DELETE FROM produto WHERE id=?', [id])
+    .then(res => res)
+  })
+}
+
--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import createTable, { insertProduto } from './Contoller/produto.js'
+import createTable, { insertProduto, updateProduto, selectProdutos, deleteProduto } from './Contoller/produto.js'
 import express from 'express'
 
 const app = express();
@@ -10,11 +10,35 @@ app.get('/', function (req, res) {
   res.send('Olá mundo')
 })
 
+app.get('/produto', async function (req, res) {
+  let produtos = await selectProdutos()
+  res.json(produtos)
+})
+
 app.post('/produto', function(req, res) {
   insertProduto(req.body)
   res.json({
     "statuscode": 200
   })
+})
+
+app.put('/produto', function(req, res) {
+  if(req.body && !req.body.id) {
+    res.json ({
+      "statuscode": 400,
+      "msg": "Você precisa informar o id"
+    })
+  }  else {
+    updateProduto(req.body)
+    res.json({
+      "statuscode": 200
+    })
+  }
+})
+
+app.delete('/produto', function (req, res) {
+  let produto = deleteProduto(req.body.id)
+  res.json(produto)
 })
 
 app.listen(3000, () => console.log('API Rodando'))
